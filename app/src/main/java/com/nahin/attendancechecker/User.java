84d69f1cfef1b460af79b5f9cfa86d30117fc1f3
Nahin-CDR <dev.nahin95@gmail.com>
2020-01-13 17:58:38 +0600

    CardView Added

--- app/src/main/java/com/nahin/attendancechecker/User.java
+++ app/src/main/java/com/nahin/attendancechecker/User.java
@@ -3,15 +3,30 @@ package com.nahin.attendancechecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class User extends AppCompatActivity {
+
+    TextView myTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_user );
 
+        myTime = (TextView)findViewById( R.id.timeID );
 
+        Date currentTime = Calendar.getInstance().getTime();
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss aa");
+        String datetime = dateformat.format(c.getTime());
+
+        myTime.setText( datetime );
 
 
 
