4bae95bfda104ead9c283b59e97783fd42e4f27b
Nahin-CDR <dev.nahin95@gmail.com>
2020-01-13 23:22:39 +0600

    date picker added

--- app/src/main/java/com/nahin/attendancechecker/User.java
+++ app/src/main/java/com/nahin/attendancechecker/User.java
@@ -2,7 +2,12 @@ package com.nahin.attendancechecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -11,25 +16,66 @@ import java.util.Date;
 
 public class User extends AppCompatActivity {
 
+    //code for time starts
     TextView myTime;
+    //code for time ends
+
+
+    //code for date starts
+    DatePickerDialog picker;
+    EditText dateInput;
+    //code for date ends
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_user );
 
+        codeForTime();
+
+        codeForDatePicker();
+
+
+
+    }
+
+    private void codeForDatePicker() {
+
+        //Code for date starts
+
+
+        dateInput =(EditText) findViewById(R.id.date_id);
+        dateInput.setInputType(InputType.TYPE_NULL);
+
+        dateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(User.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dateInput.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        //Code for date ends
+    }
+
+    private void codeForTime() {
+
         myTime = (TextView)findViewById( R.id.timeID );
-
-        Date currentTime = Calendar.getInstance().getTime();
-
         Calendar c = Calendar.getInstance();
         SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss aa");
         String datetime = dateformat.format(c.getTime());
-
         myTime.setText( datetime );
-
-
-
 
     }
 }
