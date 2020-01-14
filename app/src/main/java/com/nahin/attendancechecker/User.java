e4005a319defed46e2d612ee5af81da4fbff9759
Nahin-CDR <dev.nahin95@gmail.com>
2020-01-14 13:55:20 +0600

    userActivity Completed

--- app/src/main/java/com/nahin/attendancechecker/User.java
+++ app/src/main/java/com/nahin/attendancechecker/User.java
@@ -1,43 +1,72 @@
 package com.nahin.attendancechecker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+
 
 public class User extends AppCompatActivity {
 
     //code for time starts
     TextView myTime;
     //code for time ends
-
+    FirebaseAuth mAuth;
 
     //code for date starts
     DatePickerDialog picker;
     EditText dateInput;
     //code for date ends
 
+    Button submitButton;
+    int checkStatus;
+
+
+
+   // private ValueEventListener eventListener; //new
+    private FirebaseDatabase database = FirebaseDatabase.getInstance(); //new
+
+    EditText phoneNumber,date,name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_user );
 
+        mAuth = FirebaseAuth.getInstance();
+
         codeForTime();
 
         codeForDatePicker();
 
+        submitInformation();
+    }
 
+
+    private void codeForTime() {
+
+        myTime = (TextView)findViewById( R.id.timeID );
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss aa");
+        String datetime = dateformat.format(c.getTime());
+        myTime.setText( datetime );
 
     }
+
 
     private void codeForDatePicker() {
 
@@ -59,7 +88,7 @@ public class User extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                dateInput.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                dateInput.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
                             }
                         }, year, month, day);
                 picker.show();
@@ -69,13 +98,66 @@ public class User extends AppCompatActivity {
         //Code for date ends
     }
 
-    private void codeForTime() {
 
-        myTime = (TextView)findViewById( R.id.timeID );
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss aa");
-        String datetime = dateformat.format(c.getTime());
-        myTime.setText( datetime );
+
+
+    private void submitInformation() {
+
+
+
+        submitButton = (Button)findViewById( R.id.submit_button_id );
+        submitButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkStatus = 0;
+
+            phoneNumber = (EditText)findViewById( R.id.phone_numberID );
+            String getPhoneNumber = phoneNumber.getText().toString();
+
+            date = (EditText)findViewById( R.id.date_id );
+            String getDate = date.getText().toString();
+
+            name = (EditText)findViewById( R.id.nameID );
+            String getName = name.getText().toString();
+
+            myTime = (TextView)findViewById( R.id.timeID );
+            String inTime = myTime.getText().toString();
+
+                if(getPhoneNumber.length()!=0 && getDate.length()!=0 && getName.length()!=0){
+
+                    DatabaseReference requestRf =  database.getReference(getPhoneNumber);
+
+                    SendData sendData = new SendData(getName,getDate, getPhoneNumber,inTime,checkStatus);
+                   // SendData sendData1 = new SendData( inTime );
+
+
+                    requestRf.child(getDate).setValue(sendData);
+
+
+                    Toast.makeText(User.this, "Submitted Successfully", Toast.LENGTH_SHORT).show();
+                    date.setText("");
+                    phoneNumber.setText("");
+                    name.setText("");
+
+                }
+                else{
+
+                    phoneNumber.setError("Phone Number field Empty !");
+                    name.setError( "Name Field Empty !" );
+                }
+
+
+
+
+            }
+        });
+
+
+
 
     }
+
+
+
 }
